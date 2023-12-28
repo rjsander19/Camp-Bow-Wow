4a0a67bdb75448a478d76637c2c4d78fe04bee48
rjsander <rjsander19@gmail.com>
2023-12-27 19:03:11 -0700

    updates server.js

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ require('dotenv').config()
 
 mongoose.connect(process.env.DATABASE_URL)
 
-const db = mongoose.connectiondb.on('error', (err) => console.log(err.message + ' is mongo not running?'));
-db.on('connect', () => console.log('mongo connected'));
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 const app = express()
