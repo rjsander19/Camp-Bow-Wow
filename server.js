22626da8356b16d4d17895242dde255f43000513
rjsander <rjsander19@gmail.com>
2024-01-05 16:43:11 -0700

    env file

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
-//Configure ejs:
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); 
-
 
 //INDUCES
 
